97ac444fbe8b771c26aa4c3b54e127c047536480
Vivek <vivekkumar@kaybeeexports.com>
2026-09-15 17:04:35 +0530

    deploy first step done

--- ThinkSpace/settings.py
+++ ThinkSpace/settings.py
@@ -8,10 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY",'django-insecure-&o4aef(t94!2(hh(v*aruvh$2oc)f#)dhjqi=rq+w8)hmsp6)+')
 
-# DEBUG = os.getenv('DEBUG',True)
-DEBUG = True
+DEBUG = os.getenv('DEBUG',True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS',"127.0.0.1").split(",")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
